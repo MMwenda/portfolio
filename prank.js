7af51261d981ae0c1afed3b88146e4836336e6ba
MMwenda <maur1cemmwenda@gmail.com>
2025-02-23 12:06:07 +0100

    Updates

--- prank.js
+++ prank.js
@@ -6,22 +6,22 @@ document.getElementById("prankButton").addEventListener("click", function() {
             "Did you really just click that?",
             "Attention everyone: We got a genius here!",
             "Bro, your IQ just dropped like 20 points.",
-            "That is if you had any to begin with.",
+            "That's if you had any to begin with.",
             "Error 404: Common sense not found.",
             "I bet you also microwave ice cream.",
             "STOP. Just stop. Go touch grass.",
             "Be for real, you’re not that dumb.",
             "I take that back, you are.",
             "Looking for signs of brain cells...",
-            "...still searching.",
+            "...still looking.",
             "Imagine failing an IQ test with one question.",
             "You’re the reason we have warning labels.",
             "Allow it man, just allow it.",
             "I’m sorry, I can’t help you.",
             "I’m not mad, just disappointed.",
             "You’re a special kind of stupid.",
-            "I’m not saying you’re dumb, but...",
-            "proof of your brain's existence will be defined by what will be displayed next"
+            "I’m not saying you’re dumb...",
+            "but it seems your brain's existence is..."
     ];
     
     let prankMessage = document.getElementById("prankMessage");
